a11f5e21adefb8217946922686b4a4322aa963a1
Hack Reactor Students <craftjk@gmail.com>
2014-06-04 16:00:40 -0700

    Finished Pseudo Classical version of Stack & Queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
 };
 
+Queue.prototype.size = function () {
+  return this.length;
+};
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.length] = value;
+  this.length++;
+}
+
+Queue.prototype.dequeue = function () {
+  var result = this.storage[0];
+  if (this.length > 0) {
+    for (var i = 1; i < this.length; i++) {
+      this.storage[i-1] = this.storage[i];
+    }
+    this.length--;
+    delete this.storage[this.length];
+  }
+
+  return result;
+}
